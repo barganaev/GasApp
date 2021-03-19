e038e6522a3fe87bba8ecca573e7f06ffdf3e197
Assan Shakabayev <ashakabayev@gmail.com>
2021-03-19 13:20:51 +0500

    add login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'login.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: LogIn(),
     );
   }
 }
