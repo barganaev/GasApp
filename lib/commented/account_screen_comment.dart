c0e5f19da4dd37a8fbbf2a224246eb5485c5c91d
barganaev <zbarganaev@gmail.com>
2021-03-26 13:05:23 +0600

    changed: account page, added: alert + massive: isActive true false

--- lib/commented/account_screen_comment.dart
+++ lib/commented/account_screen_comment.dart
@@ -1,3 +1,5 @@
+// ACCOUNT SCREEN (CARD VARIANT)
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gasapp/utils/constants.dart';
@@ -9,13 +11,13 @@ class AccountScreen extends StatefulWidget {
 
 class _AccountScreenState extends State<AccountScreen> {
 
-  List<bool> valuesOfButton = [];
+  List<int> isOpen = [0, 1, 0, 1];
 
   bool switchState = false;
 
   bool falseBooleanCreatedForCheckingTheStation = false;
 
-  Widget Status(int index){
+  Widget Status(){
     // for (int i = 0; i < isOpen.length; i++) {
     //   if(isOpen[i] == 1){
     //     return Text('Открыт', style: TextStyle(color: Colors.green),);
@@ -23,7 +25,7 @@ class _AccountScreenState extends State<AccountScreen> {
     //     return Text('Закрыт', style: TextStyle(color: Colors.red),);
     //   }
     // }
-    if(/*switchState*/ valuesOfButton[index]){
+    if(falseBooleanCreatedForCheckingTheStation){
       return Text('Открыт', style: TextStyle(color: Colors.green),);
     }else{
       return Text('Закрыт', style: TextStyle(color: Colors.red),);
@@ -46,8 +48,8 @@ class _AccountScreenState extends State<AccountScreen> {
         centerTitle: true,
         title: Text('Личный кабинет', style: TextStyle(color: Colors.blue),),
         actions: [
-        IconButton(
-            icon: Icon(Icons.account_circle, color: Colors.blue),
+          IconButton(
+            icon: Icon(Icons.location_city, color: Colors.blue),
             onPressed: () {
               Navigator.popUntil(context, (Route<dynamic> route) => route.isFirst);
               // Navigator.push(context,
@@ -70,152 +72,157 @@ class _AccountScreenState extends State<AccountScreen> {
             Expanded(
               child: Container(
                 child: ListView.builder(
-                    itemCount: 10,
+                    itemCount: isOpen.length,
                     itemBuilder: (context, i){
-                      valuesOfButton.add(false);
-                      // valuesOfButton[i] = false;
-                      return Container(
-                        // decoration: BoxDecoration(
-                        //   borderRadius: BorderRadius.circular(10),
-                        //   boxShadow: [
-                        //     BoxShadow(
-                        //       color: Colors.grey,
-                        //       spreadRadius: 5,
-                        //       blurRadius: 7,
-                        //       offset: Offset(0,3)
-                        //     )
-                        //   ]
-                        // ),
-                        //color: Colors.red,
-                        //height: screenSize(context).height * 0.1,
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Container(
-                                  //color: Colors.green,
-                                  width: screenSize(context).width * 0.5,
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text('АГЗС LPG Trade', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.orange),),
-                                      Text('Местонахождение: город Актау, микрорайон 21, промышленная база', style: TextStyle(fontSize: 15),),
-                                      Text('Показать на карте', style: TextStyle(decoration: TextDecoration.underline, color: Colors.blue),),
-                                      // Container(
-                                      //   color: Colors.lightGreen[200],
-                                      //   child: Row(
-                                      //     children: [
-                                      //       Text('Открыть или закрыть объект'),
-                                      //       CupertinoSwitch(
-                                      //         value: switchState,
-                                      //         onChanged: (bool value) {
-                                      //           setState(() {
-                                      //             switchState = value;
-                                      //           });
-                                      //           print(value);
-                                      //         }),
-                                      //
-                                      //     ],
-                                      //   ),
-                                      // )
-                                    ],
+                      return Card(
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: screenSize(context).height * 0.03, horizontal: screenSize(context).width * 0.05),
+                          // decoration: BoxDecoration(
+                          //   borderRadius: BorderRadius.circular(10),
+                          //   boxShadow: [
+                          //     BoxShadow(
+                          //       color: Colors.grey,
+                          //       spreadRadius: 5,
+                          //       blurRadius: 7,
+                          //       offset: Offset(0,3)
+                          //     )
+                          //   ]
+                          // ),
+                          //color: Colors.red,
+                          //height: screenSize(context).height * 0.1,
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    //color: Colors.green,
+                                    width: screenSize(context).width * 0.5,
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text('АГЗС LPG Trade', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.orange),),
+                                        Text('Местонахождение: город Актау, микрорайон 21, промышленная база', style: TextStyle(fontSize: 15),),
+                                        Text('Показать на карте', style: TextStyle(decoration: TextDecoration.underline, color: Colors.blue),),
+                                        // Container(
+                                        //   color: Colors.lightGreen[200],
+                                        //   child: Row(
+                                        //     children: [
+                                        //       Text('Открыть или закрыть объект'),
+                                        //       CupertinoSwitch(
+                                        //         value: switchState,
+                                        //         onChanged: (bool value) {
+                                        //           setState(() {
+                                        //             switchState = value;
+                                        //           });
+                                        //           print(value);
+                                        //         }),
+                                        //
+                                        //     ],
+                                        //   ),
+                                        // )
+                                      ],
+                                    ),
                                   ),
-                                ),
 
-                                Padding(
-                                  padding: EdgeInsets.only(right: screenSize(context).width * 0.03),
-                                  child: Column(
+                                  //Padding(
+                                  //padding: EdgeInsets.only(right: screenSize(context).width * 0.03),
+                                  /*child:*/ Column(
                                     children: [
                                       Container(
                                         // width: 200,
                                         //color: Colors.blue,
                                           child: Text('Сейчас: ')
                                       ),
-                                      Status(i)
+                                      Status()
+                                    ],
+                                  ),
+                                  //),
+
+                                  // Expanded(
+                                  //   child: Container(
+                                  //     // width: 200,
+                                  //     //color: Colors.blue,
+                                  //     child: Text('Сейчас: ')
+                                  //   ),
+                                  // ),
+                                  // Status()
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: screenSize(context).height * 0.02),
+                                child: Container(
+                                  //color: Colors.lightGreen[200],
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                          padding: EdgeInsets.only(left: screenSize(context).width * 0.03),
+                                          //width: screenSize(context).width * 0.6,
+                                          child: Text(
+                                              'Открыть или закрыть объект'
+                                          )
+                                      ),
+                                      /*Padding(
+                                        padding: EdgeInsets.only(right: screenSize(context).width * 0.03),
+                                        child:*/ CupertinoSwitch(
+                                          trackColor: Colors.red,
+                                          activeColor: Colors.green,
+                                          value: switchState,
+                                          onChanged: (bool value) {
+                                            showDialog(
+                                                context: context,
+                                                builder: (_) => CupertinoAlertDialog(
+                                                  title: Text('Вы уверены?'),
+                                                  actions: [
+                                                    GestureDetector(
+                                                      onTap: (){
+                                                        Navigator.pop(context);
+                                                        // switchState = false;
+                                                        // falseBooleanCreatedForCheckingTheStation = false;
+                                                      },
+                                                      child: CupertinoDialogAction(
+                                                        child: Text('No'),
+                                                      ),
+                                                    ),
+                                                    GestureDetector(
+                                                      onTap: (){
+                                                        Navigator.pop(context);
+                                                        switchState = value;
+                                                        falseBooleanCreatedForCheckingTheStation = value;
+                                                      },
+                                                      child: CupertinoDialogAction(
+                                                        child: Text('Yes'),
+                                                      ),
+                                                    )
+                                                  ],
+                                                ),
+                                                barrierDismissible: false
+                                            ).then((value) {
+                                              setState(() {});
+                                            });
+                                            //switchState = value;
+                                            //falseBooleanCreatedForCheckingTheStation = value;
+                                            print(value);
+                                          }
+                                      ),
+                                      //),
+
                                     ],
                                   ),
                                 ),
-
-                                // Expanded(
-                                //   child: Container(
-                                //     // width: 200,
-                                //     //color: Colors.blue,
-                                //     child: Text('Сейчас: ')
-                                //   ),
-                                // ),
-                                // Status()
-                              ],
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: screenSize(context).height * 0.02),
-                              child: Container(
-                                //color: Colors.lightGreen[200],
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Container(
-                                      padding: EdgeInsets.only(left: screenSize(context).width * 0.03),
-                                      //width: screenSize(context).width * 0.6,
-                                      child: Text(
-                                        'Открыть или закрыть объект'
-                                      )
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.only(right: screenSize(context).width * 0.03),
-                                      child: CupertinoSwitch(
-                                        trackColor: Colors.red,
-                                        activeColor: Colors.green,
-                                          value: valuesOfButton[i],
-                                          onChanged: (bool value) {
-                                              showDialog(
-                                                  context: context,
-                                                  builder: (_) => CupertinoAlertDialog(
-                                                    title: Text('Вы уверены?'),
-                                                    actions: [
-                                                      GestureDetector(
-                                                        onTap: (){
-                                                          Navigator.pop(context);
-                                                          // switchState = false;
-                                                          // falseBooleanCreatedForCheckingTheStation = false;
-                                                        },
-                                                        child: CupertinoDialogAction(
-                                                          child: Text('No'),
-                                                        ),
-                                                      ),
-                                                      GestureDetector(
-                                                        onTap: (){
-                                                          Navigator.pop(context);
-                                                          valuesOfButton[i] = value;
-                                                          falseBooleanCreatedForCheckingTheStation = value;
-                                                        },
-                                                        child: CupertinoDialogAction(
-                                                          child: Text('Yes'),
-                                                        ),
-                                                      )
-                                                    ],
-                                                  ),
-                                                  barrierDismissible: false
-                                              ).then((value) {
-                                                setState(() {});
-                                              });
-                                              //switchState = value;
-                                              //falseBooleanCreatedForCheckingTheStation = value;
-                                            print(value);
-                                          }
-                                          ),
-                                    ),
-
-                                  ],
-                                ),
                               ),
-                            ),
-                            //Container(),
-                            Padding(
-                              padding: EdgeInsets.symmetric(vertical: screenSize(context).height * 0.02),
-                              child: Divider(color: Colors.black,),
-                            )
-                          ],
+                              //Container(),
+                              // Padding(
+                              //   padding: EdgeInsets.symmetric(vertical: screenSize(context).height * 0.02),
+                              //   child: Divider(),
+                              // )
+                            ],
+                          ),
                         ),
                       );
                     }
