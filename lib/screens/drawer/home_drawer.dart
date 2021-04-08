8bec4522ce01cde104f35b356a557c8c512ea7e9
barganaev <zbarganaev@gmail.com>
2021-04-08 11:35:12 +0600

    changed: regenerated app icon, deleted: name

--- lib/screens/drawer/home_drawer.dart
+++ lib/screens/drawer/home_drawer.dart
@@ -48,17 +48,17 @@ class _HomeDrawerState extends State<HomeDrawer> {
                 // height: 50,
                 fit: BoxFit.fill,
               ),
-              title: Text(
-                "АГЗС",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Montserrat',
-                    color: Color(0xFF016fbe) /*Colors.black87*/,
-                    fontSize: 20),
-              ),
+              // title: Text(
+              //   "АГЗС",
+              //   style: TextStyle(
+              //       fontWeight: FontWeight.bold,
+              //       fontFamily: 'Montserrat',
+              //       color: Color(0xFF016fbe) /*Colors.black87*/,
+              //       fontSize: 20),
+              // ),
               subtitle: Text(
                 "Газозаправочные станции Мангистауской области",
-                style: TextStyle(color: Color(0xFF016fbe), fontSize: 12),
+                style: TextStyle(color: Color(0xFF016fbe), fontSize: 15/*12*/),
               ),
               //trailing: Icon(Icons.arrow_right_sharp),
             ),
